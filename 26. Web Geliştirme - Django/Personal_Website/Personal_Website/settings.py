35b46be26e2875c130dd0741e926d16e4e11546e
Oğuzhan <56503078+ouzhan-uysal@users.noreply.github.com>
2021-09-18 16:52:20 +0300

    Update settings.py

--- 26. Web Geliştirme - Django/Personal_Website/Personal_Website/settings.py
+++ 26. Web Geliştirme - Django/Personal_Website/Personal_Website/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tkrcyv_maw(s!!jyo1ed=dk276_@0unbfn_xiw29d(q%rxmnds'
+SECRET_KEY = 'WRITE_SECRET_KEY_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
